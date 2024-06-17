049bc91f936aa62cd91c8d89559572b06575df6f
Sumanth Kumar CP <chennapandarisumanthkumar@gmail.com>
2024-06-17 14:02:40 +0530

    Completed first lecture of javascript

---
+++ javascript/first.js
@@ -0,0 +1,50 @@
+//console.log("Hello Sumanth....");
+//variables in JS
+name = "Tony Stark";
+console.log(name);
+age = 24;
+console.log(age);
+price = 150.50;
+console.log(price);
+Num = null;
+console.log(Num);
+N = undefined;
+console.log(N);
+isfollow = false;
+console.log(isfollow);
+is = true;
+console.log(is);
+
+
+let fullname = "Sumanth Kumar";
+console.log(fullname);
+
+var Fullname = "Sumanth Kumar";
+console.log(Fullname);
+
+const NAME = "Tony Stark";
+console.log(NAME);
+
+let hi;
+hi = "Hello";
+console.log(hi);
+
+let x = BigInt(123);
+let y = Symbol("HEllo!");
+
+console.log(x , y);
+
+const Student = {
+    namee : "Sumanth Kumar",
+    age : 21,
+    CGPA : 7.78,
+    isPass : true
+}
+
+console.log(Student);
+
+console.log(Student["namee"]);
+console.log(Student.namee);
+
+Student.age = Student.age + 1;
+console.log(Student.age);
---
+++ javascript/test1.js
@@ -0,0 +1,18 @@
+//problem statement - solution 
+//store The detials of the product from an amazon website 
+// Problem is to store The instagram profile of The given person
+
+const Profile = {
+    name : "Shradha Khapra",
+    handle : "shradhakhapra",
+    Follow : false,
+    Message : "Link",
+    Number_of_followers : 195000,
+    NUmber_of_posts : 195,
+    Bio : "Apna Collge | Ex-Microsoft, DRDO \n To educate someone is the higest previlege",
+    Number_of_following : 4,
+    Job : "Entrepreneur",
+    Threads_handle : "@shradhakhapra"
+}
+
+console.log(Profile);
