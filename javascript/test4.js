59eccb6094182f08eddd50f781375ec33911291b
Sumanth Kumar C P <chennapandarisumanthkumar@gmail.com>
2024-06-18 15:35:15 +0530

    Added new files

---
+++ javascript/test4.js
@@ -0,0 +1,20 @@
+// print all even numbers from 0 to 100
+
+// for(let i =0 ; i <= 100 ; i++)
+//     {
+//         if(i%2 == 0)
+//             {
+//                 console.log(i);
+//             }
+//     }
+
+// guess the number Game
+
+let number = 25;
+let num = 0;
+while(num != number)
+    {
+        num = prompt("Guess the Number....?");
+    }
+
+console.log("Congrats you have gussed the correct number");
