28b80c736bd98217fba1d12f3e2506176e14e717
Sumanth Kumar C P <chennapandarisumanthkumar@gmail.com>
2024-06-20 22:46:07 +0530

    Added first JS mini project -tic tak toe game

---
+++ javascript/miniprojects/app.js
@@ -0,0 +1,55 @@
+let boxes = document.querySelectorAll(".box");
+let resetbtn = document.querySelector(".Resetbutton");
+
+let turn0 = true;//palyerO
+
+const winPatterns = [
+    [0,1,2],
+    [0,3,6],
+    [0,4,8],
+    [1,4,7],
+    [2,5,8],
+    [2,4,6],
+    [3,4,5],
+    [6,7,8]
+];
+
+boxes.forEach((box) => 
+{
+    box.addEventListener("click" , () => 
+    {
+        console.log("box was clicked");
+        //box.innerText = "A";
+        if(turn0 === true)
+            {
+                box.innerText = "O";
+                turn0 = false;
+            }
+            else
+            {
+                box.innerText = "X";
+                turn0 = true;
+            }
+            box.disabled = true;
+
+            checkWinner();
+    });
+});
+
+const checkWinner = () => 
+    {
+        for(let patterns of winPatterns)
+            {
+                let pos0val = boxes[patterns[0]].innerText;
+                let pos1val = boxes[patterns[1]].innerText;
+                let pos2val = boxes[patterns[2]].innerText;
+
+                if(pos0val != "" && pos0val != "" && pos0val != "")
+                    {
+                        if(pos0val === pos1val && pos1val === pos2val)
+                            {
+                                //alert(`Winner is ${pos0val}`);
+                            }
+                    }
+            }
+    };
